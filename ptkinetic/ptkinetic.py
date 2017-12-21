f25dd6ebc8357732e0aeb508258e5e7a039da50c
zeldery <zeldery@yahoo.com.vn>
2017-12-21 13:36:05 +0700

    Update the cycle feature
    Add the num_of_cycle to reduce the amount of memory used to store the value

--- ptkinetic/ptkinetic.py
+++ ptkinetic/ptkinetic.py
@@ -13,137 +13,117 @@ from matplotlib.pyplot import cm
 
 class Kinetic:
     '''The main class for Kinetic
-including 4 basic steps:
-+ add chemicals
-+ add reactions
-+ init
-+ run'''
-    def __init__(self,delta = None, check = False, criterion = None):
+'''
+    def __init__(self):
         '''Variable declaration'''
-        self.delta = delta # the time step, must be small enough
-        self.check = check # Whether check the criterion or not
-        self.criterion = criterion # the criterion of the ratio between the change and current concentration of any chemicals
-        self.status = 0 # 1 for add chemical, 2 for add reaction, 3 for initialization, 4 for already run
+        self.delta = 0.0 # the time step, must be small enough
+        self.check = False # Whether check the criterion or not
+        self.criterion = 1.0 # the criterion of the ratio between the change and current concentration of any chemicals
+        self.num_of_steps = 1
+        
+        # Flag variables
+        self.flag_init = False
+        self.flag_run = False
         # Chemical properties
         self.chemicals = [] # Contain the name of reaction
         self.concentrations = [] # Contain the initial concentration
-        self.stables = [] # True if the concentration does not change during the simulation
+        self.chemical_stables = [] # True if the concentration does not change during the simulation
         # Reaction properties
         self.reaction_inputs = [] # List of reactants
         self.reaction_outputs = [] # List of products
         self.reaction_constants = [] # List of rate constants
+        
+        # Data variables
+        self.times = None
+        self.data = None
+        self.k = None
         # Other properties
         self.fig = [] # List of figure object, for clean-up only
         
         
     def add_chemical(self,name,concentration,stable = False):
         '''Add chemical nomenclature to the model'''
-        if self.status == 0:
-            self.status = 1 # Update the status
-        if self.status == 1:
-            if name in self.chemicals:
-                raise ValueError('The chemical name is already in the Kinetic object')
-            self.chemicals += [name]
-            self.concentrations += [concentration]
-            self.stables += [stable]
-        else: # Error if already add a reaction
-            raise RuntimeError('Cannot add chemical after adding reaction')
+        if self.flag_init:
+            raise RuntimeError('Cannot add chemical after initialized')
+        if name in self.chemicals:
+            raise ValueError('The chemical name is already in the Kinetic object')
+        self.chemicals += [name]
+        self.concentrations += [concentration]
+        self.chemical_stables += [stable]
     
     
     def add_reaction(self, inp, outp, constant):
         '''Add reactions to model, including reactants, products and rate constant'''
-        if self.status == 1:
-            self.status = 2
-        if self.status == 2:
-            # Ensure that all the names are valid
-            for name in inp:
-                if name not in self.chemicals:
-                    raise ValueError('The chemical name has not been added')
-            for name in outp:
-                if name not in self.chemicals:
-                    raise ValueError('The chemical name has not been added')
-            self.reaction_inputs += [inp]
-            self.reaction_outputs += [outp]
-            self.reaction_constants += [constant]
-        elif self.status == 0:
-            raise RuntimeError('Cannot add reaction before adding chemical') # Check for right sequence
-        else:
-            raise RuntimeError('Cannot add reaction after running initialization step')
+        if self.flag_init:
+            raise RuntimeError('Cannot add reaction after initialized')
+        for name in inp:
+            if name not in self.chemicals:
+                raise ValueError('The chemical name has not been added')
+        for name in outp:
+            if name not in self.chemicals:
+                raise ValueError('The chemical name has not been added')
+        self.reaction_inputs += [inp]
+        self.reaction_outputs += [outp]
+        self.reaction_constants += [constant]
     
-    def init(self, delta = None, check = None, criterion = None):
+    def init(self, num_of_steps = 1, check = False, criterion = None):
         '''Initialize the variable necessary in the simulation'''
-        if self.status == 2:
-            if delta != None:
-                self.delta = delta
-            if criterion != None:
-                self.criterion = criterion
-            if check != None:
-                self.check = check
-            if self.check == False:
-                if self.delta == None:
-                    raise ValueError('Missing step size')
-            elif self.delta == None or self.criterion == None:
-                raise ValueError('Missing step size or criterion') # Check for missing required parameters
-            self.status = 3
-            n_chem = len(self.chemicals)
-            n_react = len(self.reaction_constants)
-            self.k = np.array(self.reaction_constants) * self.delta # The constant with the delta multiplied in advance
-            self.inp_marker = np.zeros((n_react,n_chem)) # A matrix, each row is an reaction, each column is an chemical
-            # Mark the reactant 1 for each time they appear
-            for i in range(n_react):
-                for name in self.reaction_inputs[i]:
-                    self.inp_marker[i,self.chemicals.index(name)] += 1
-            # The same with products
-            self.outp_marker = np.zeros((n_react,n_chem))
-            for i in range(n_react):
-                for name in self.reaction_outputs[i]:
-                    self.outp_marker[i,self.chemicals.index(name)] += 1
-            self.data = np.array(self.concentrations).reshape((1,n_chem)) # Matrix with each row is run, each column is a chemical
-            self.stables = np.array(self.stables) # Convert to numpy object
-        else:
-            if self.status < 2: # Check the right sequence
-                raise RuntimeError('Cannot initialize before adding reaction')
-            else:
-                raise RuntimeError('Already initialized before')
+        if self.flag_init:
+            raise RuntimeError('Cannot reinitialize')
+        if check and criterion == None:
+            raise ValueError('Criterion is not given to check')
+        self.check = check
+        self.criterion = criterion
+        self.flag_init = True
+        self.num_of_steps = num_of_steps
+        n_chem = len(self.chemicals)
+        n_react = len(self.reaction_constants)
+        self.k = np.array(self.reaction_constants) # The constant with the delta multiplied in advance
+        self.inp_marker = np.zeros((n_react,n_chem)) # A matrix, each row is an reaction, each column is an chemical
+        # Mark the reactant 1 for each time they appear
+        for i in range(n_react):
+            for name in self.reaction_inputs[i]:
+                self.inp_marker[i,self.chemicals.index(name)] += 1
+        # The same with products
+        self.outp_marker = np.zeros((n_react,n_chem))
+        for i in range(n_react):
+            for name in self.reaction_outputs[i]:
+                self.outp_marker[i,self.chemicals.index(name)] += 1
+        self.times = np.array([0.0])
+        self.data = np.array(self.concentrations).reshape((1,n_chem)) # Matrix with each row is run, each column is a chemical
+        self.stables = np.array(self.chemical_stables) # Convert to numpy object
+        
+
     
-    def run(self,times):
-        if self.status == 3: # If run for the first time
-            n_react, n_chem = self.inp_marker.shape
-            self.status = 4
-            self.data = np.zeros((times+1, n_chem)) # Create empty data
-            self.data[0,:] = np.array(self.concentrations)
-            for i in range(times):
-                rate = ( self.data[i,:] ** self.inp_marker ).prod(axis=1) * self.k # Rate formula in numpy format
-                                                                                   # Using numpy broadcasting rules
+    def run(self, cycle, delta):
+        ''' Run function '''
+        if not self.flag_init:
+            raise RuntimeError('You have to initialize before run')
+        self.flag_run = True
+        k = self.k * delta / self.num_of_steps
+        current, n_chem = self.data.shape
+        temp = self.times[-1] + np.arange(1,cycle+1)*delta
+        self.times = np.concatenate((self.times, temp), axis = 0)
+        temp = np.zeros((cycle, n_chem))
+        self.data = np.concatenate((self.data, temp), axis = 0)
+        temp = self.data[current-1,:]
+        for i in range(cycle):
+            for j in range(self.num_of_steps):
+                rate = (temp ** self.inp_marker).prod(axis=1) * k
                 change_outp = (self.outp_marker.T * rate).sum(axis = 1)
                 change_inp = (self.inp_marker.T * rate).sum(axis = 1)
                 change = np.where(self.stables, 0.0, change_outp - change_inp) # Do not change if the concentration is stable
                 # Check if the criterion is violated
                 # Only check for consumed amount, not forming amount
-                if self.check and np.where(self.data[i,:] != 0, np.abs(change_inp/self.data[i,:]) , 0.0).max() > self.criterion:
+                if self.check and np.where(temp != 0, np.abs(change_inp/temp) , 0.0).max() > self.criterion:
                     raise ValueError('The change in one step is too large, decrease the step size')
-                self.data[i+1,:] = self.data[i,:] + change
-        elif self.status == 4: # If already run
-            n_react, n_chem = self.inp_marker.shape
-            data = np.zeros((times+1,n_chem)) # Create different data
-            data[0,:] = self.data[-1,:] # Copy the last simulation concentration
-            for i in range(times):
-                # The same as the case of status=3
-                rate = ( data[i,:] ** self.inp_marker ).prod(axis=1) * self.k
-                change_outp = (self.outp_marker.T * rate).sum(axis = 1)
-                change_inp = (self.inp_marker.T * rate).sum(axis = 1)
-                change = np.where(self.stables, 0.0, change_outp - change_inp)
-                if self.check and np.where(data[i,:] !=0 , np.abs(change_inp/data[i,:]) , 0.0).max() > self.criterion:
-                    raise ValueError('The change in one step is too large, decrease the step size')
-                data[i+1,:] = data[i,:] + change
-            self.data = np.concatenate((self.data,data[1:,:]),axis = 0) # Append the data to self.data
-        if self.status < 3:
-            raise RuntimeError('Cannot run before initialization')
+                temp = temp + change
+            self.data[current,:] = temp
+            current += 1
     
     def reset(self):
         '''Free the data and figure, keep only the last concentration'''
         self.data = self.data[-1,:]
-        self.status = 3
         for fig in self.fig:
             plt.close(fig)
             
@@ -154,9 +134,9 @@ including 4 basic steps:
         
     def plot(self, chemical = None):
         ''' Plot the concentration '''
-        if self.status < 4:
-            raise RuntimeError('Cannot plot before run')
-        n_run, n_chem = self.data.shape
+        if not self.flag_run:
+            raise RuntimeError('You have to run before plot')
+        n_chem = self.data.shape[1]
         fig = plt.figure()
         ax = fig.add_subplot(1,1,1)
         if chemical == None:
@@ -165,7 +145,7 @@ including 4 basic steps:
             ind = [self.chemicals.index(x) for x in chemical] # Plot the concentration given by chemical argument
         color_map = cm.rainbow(np.linspace(0,1,len(ind))) # Continuous rainbow color
         for i,j in enumerate(ind):
-            temp, = ax.plot(self.delta*np.arange(n_run), self.data[:,j], c=color_map[i],label = self.chemicals[j])
+            temp, = ax.plot(self.times, self.data[:,j], c=color_map[i],label = self.chemicals[j])
         ax.legend()
         fig.show()
         self.fig += [fig] # Save the figure to delete
